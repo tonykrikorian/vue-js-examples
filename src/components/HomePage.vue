91cb78aee5d18ea290ab7a2712beeadf0449b3f1
Tony Krikorian <tony.krikorian@outlook.com>
2021-12-01 21:59:06 -0300

    Add HomePage component

---
+++ src/components/HomePage.vue
@@ -0,0 +1,30 @@
+/* eslint-disable quotes */
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" alt="" aria-hidden="true" />
+    </div>
+    <div class="get-started"><a href="">Get Started</a> Building your first robot!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line quotes
+  name: "HomePage",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+
+.robot {
+  height: 300px;
+}
+</style>
